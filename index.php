467a3ec1b36c8f378abd0bf1df466c30934ae4de
nick <beerpriceindex@gmail.com>
2015-12-21 08:53:17 -0500

    updates

--- index.php
+++ index.php
@@ -23,12 +23,11 @@
 				</p>
 				<h1> 15 seconds could save you 15 percent or more! </h1>
 				<p>It's amazing that two stores less than 5 minutes apart can have dramatic differences in prices. Our research indicates that prices can vary
-				by 20% for the same case of beer in a single geographic location. cheapbeerprices.com aims to inform you which store is the cheapest for 
-				the beverage that you are looking for. </p>
+				by 20% for the same case of beer in a single geographic location. Cheapbeerprices.com aims to guide you to find the cheapest cheapest beverages, so you can save money. </p>
 				
 				<h3> Let us know what you paid! </h3>
 				<p> Beer buddy is a community that relies on people like you to share how much you paid for a case of brew.
-				Let us know how much they paid for a case a specific vendor, and we will publish it on our website.<p>
+				Let us know how much you paid for a case a specific vendor, and we will publish it on our website.<p>
 				<p> Email the location, price, and type of beer to:</p>
 				<p> beerpriceindex@gmail.com </p>
 			</article>								
