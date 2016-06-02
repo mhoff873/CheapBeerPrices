2c5498b5602f3c58bb5478b2b42c4a81fd9fca22
nick <beerpriceindex@gmail.com>
2016-06-01 22:01:38 -0400

    modified:   storeProducts.php 	deleted:    storeSummary.php

--- storeProducts.php
+++ storeProducts.php
@@ -1,5 +1,9 @@
 <?php 
 	include 'header.php';
+	//RETURNS TRUE WHEN COOKIES ARE SET
+	//function cookies(){
+	//	return (isset($_COOKIE['time_zone_offset'],$_COOKIE['time_zone_dst']));
+	//}
 	function db_connect1(){
 		$db_host = 'localhost';
 		$db_user= 'root';
@@ -19,229 +23,188 @@
 		mysql_close($connection);
 	}
 	db_connect1(); //CONNECT TO DATABASE RIGHT AWAY
-
+	
 	$id=(isset($_GET['ID']))?$_GET['ID']:null;
 
-	$getDetails=(!is_null($id))?
-	"SELECT ID,Name,
-	address,phoneNumber,website,latitude,longitude,Location,State,County,
-	openSun,closeSun,openMon,closeMon,openTue,closeTue,
-	openWed,closeWed,openThu,closeThu,openFri,closeFri,openSat,closeSat
+	//only 1 condition is needed to decide which vendor to pull from for pulling
+	$getData=(isset($_GET['ID']))?
+	"SELECT Name,ID,Location,State
 	FROM Vendors
-	WHERE ID = ".$id."
-	AND patchHours = 1":null; 
+	WHERE ID = ".$id:null; 
 	
-	$fill = (!is_null($getDetails))?mysql_query($getDetails):null;
+	$fill = (!is_null($getData))?mysql_query($getData):null;
 
- 	$data=array();
-	$data=(!is_null($fill))?array(
-		"Id"=> mysql_result($fill,0,"ID"),
-		"name" => mysql_result($fill,0,"Name"),
-		"location"=> mysql_result($fill,0,"Location"),
-		"state"=>mysql_result($fill,0,"State"),
-		"phoneNumber"=>mysql_result($fill,0,"phoneNumber"),
-		"website"=>mysql_result($fill,0,"website"),
-		"latitude"=>mysql_result($fill,0,"latitude"),
-		"longitude"=>mysql_result($fill,0,"longitude"),
-		"county"=>mysql_result($fill,0,"County"),
-		"address" => mysql_result($fill,0,"address"),
-		"phoneNumber" => mysql_result($fill,0,"phoneNumber"),
-		"website" => mysql_result($fill,0,"website"),
-		"openMon" => mysql_result($fill,0,"openMon"),
-		"closeMon" => mysql_result($fill,0,"closeMon"),
-		"openTue" => mysql_result($fill,0,"openTue"),
-		"closeTue" => mysql_result($fill,0,"closeTue"),
-		"openWed" => mysql_result($fill,0,"openWed"),
-		"closeWed" => mysql_result($fill,0,"closeWed"),
-		"openThu" => mysql_result($fill,0,"openThu"),
-		"closeThu" => mysql_result($fill,0,"closeThu"),
-		"openFri" => mysql_result($fill,0,"openFri"),
-		"closeFri" => mysql_result($fill,0,"closeFri"),
-		"openSat" => mysql_result($fill,0,"openSat"),
-		"closeSat" => mysql_result($fill,0,"closeSat"),
-		"openSun" => mysql_result($fill,0,"openSun"),
-		"closeSun" => mysql_result($fill,0,"closeSun")):$data;
+	$id=(!is_null($fill))?mysql_result($fill,0,"ID"):$id;
+	$storeName=(!is_null($fill))?mysql_result($fill,0,"Name"):$storeName;
+	$town=(!is_null($fill))?mysql_result($fill,0,"Location"):$town;
+	$state=(!is_null($fill))?mysql_result($fill,0,"State"):$state; 
 
-	echo"	<main id='center' class='column'>
+	echo"	
+
+	
+	<main id='center' class='column'>
 	<article>
-	<h1 style='text-align:center;font-size:1.65em;'><b>".$data['name']." -  ".$data['location'].", ".$data['state'].".</b></h1>";
-	
-/* 	<ul style='width:100%;list-style-type: none;margin: 0;padding: 0;'>
-	<li class='tabLeft' style='border-bottom:none;background-color:silver;border-right:none;'><h3>Store Summary</h3></li>
-	<a href=storeProducts.php?";
-	
-	if(isset($_GET['ID'])){
-		echo"ID=".$_GET['ID'];
-	}
-	//ELSE:
+	<h1 style='text-align:center;font-size:1.65em;'><b>".$storeName." -  ".$town.", ".$state.".</b></h1>
 
-	echo"><li class='tabRight' style='background-color:#707070;color:white;height:2.38em;border-bottom:none;border-left:none;'><h3>Store Products</h3></li></a>
-	</ul>
-	<br>"; */
-	
-	echo"<div style='float:none;background-color:#707070;padding-bottom:1.25em;'>
-	
-	
+	<div style='padding: 0;border: 0;margin: 0;float:none;background-color:#707070;padding-bottom:1.25em;'>
 	<table style='width:100%;margin: 0;padding: 0;'> 
 		<tr>
-			<td class='tabLeft' style='background-color:707070;color:white;display:block;border-bottom:none;border-left:none;'>
-				
-					<h1>Store Summary</h1></a>
-
-			</td>
-			
-			<td class='tabRight' style='background-color:silver;'>
-			<a href=storeProducts.php?";
+			<td class='tabLeft' style='background-color:silver;color:white;display:block;border-bottom:none;border-left:none;'>
+				<a href=storeSummary2.php?";
 				//setting the link to correct vendor's summary page
 				if(isset($_GET['ID'])){
 					echo"ID=".$_GET['ID'];
 				} 
 				echo" style='width: 50%;height: 100%;'>
-				<h3>Store Products</h3></a>
+					<h3>Store Summary</h3></a>
+
+			</td>
+			
+			<td class='tabRight' style='background-color:#707070;'>
+				<h1 style='color:white'>Store Products</h1>
 			</td>
 		</tr>
 	</table>
 	<br>
-	
-	
-	
-	
 	<div style='background-color:silver;width:98%;margin-right:1%;margin-left:1%;padding-bottom:1em;'>
-	<br><center><div id='map' style='height:24em;width:98%;margin-right:1%;margin-left:1%;'></div>
-	<div id='info' style='height:24em;width:98%;margin-right:1%;margin-left:1%;'></div>";
-
-?>
-<script>
-	//PHP GLOBALS	
-<?php
-	echo "var data = ".json_encode($data).";\n";
-	if(isset($_GET['ID'])){
-		$vendorID = json_encode($_GET['ID']);
-		echo "var ID = ". $vendorID . ";\n";
-	}
-	//ELSE:
 	
 	
-?>
-
-days=['Sun','Mon','Tue','Wed','Thu','Fri','Sat']; //for retrieving data from database
-dayNames=['Sundat','Monday','Tuesday','Wednesday','Thusday','Friday','Saturday']; //for printing menu
-var map;
-var infowindow;
-
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 15,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	});
-	var service = new google.maps.places.PlacesService(map);
-	//if(!(typeof ID === 'undefined')){
-	map.setCenter(new google.maps.LatLng(data['latitude'], data['longitude']));
-	var opening = new Date();
-	var timeNow = '';
-	timeNow += opening.getHours();
-	timeNow += opening.getMinutes();
-	var today = opening.getDay();
-	var open = "open"+days[today];
-	var name = data['name'];
-	var close = "close"+days[today];
-	var address = data['address'];
-	var phoneNumber = data['phoneNumber'];
-	var website = data['website'];
-	if(open>close){  //opens during day, closes during the early AM hours
-		var status = (timeNow>=open||timeNow<=close)?'<img src="images/open.png" width="80%" height="auto">':'<img src="images/closed.png" width="80%" height="auto">';
-	}
-	if(close>open){  //opens during day, closing later on same day
-		var status = (timeNow>=open&&timeNow<=close)?'<img src="images/open.png" width="80%" height="auto">':'<img src="images/closed.png" width="80%" height="auto">';
-	}
-	timeText = theTimeSlingingSlasher(data[open],data[close]);
-	//table with phonenumber, open now, and website link
-	var topTableString = '<table style="text-align:center;width:96%;border-bottom:solid;border-top:solid;border-width:thin;margin-right:2%;margin-left:2;">'+
-		'<tr >'+
-		'<td style="width:33%;"><h3><a href="tel:'+phoneNumber+'">'+phoneNumber+'</a></h3></td>'+
-		'<td style="width:33%;border-right:solid;border-left:solid;border-width:thin;text-align:center"><h3>'+status+'</h3></td>'+
-		'<td style="width:33%;text-align:center"><a href="'+website+'" target="_blank"><h3>Visit their website</h3></a></td>'+
-		'</tr>'+
-		'</table>';
+	<form method='GET'>
+	<input type='hidden' name='ID' value='".$_GET['ID']."'>
+	<table style='width:96%'>
+		<tr>
+		
+		<td style='width:84%'>";
+		if(isset($_GET['search']))
+			echo"<input style='width:98%;height:2em;font-size:1.2em;font-weight:bold;margin-top:0.5em;margin-left:2%;text-indent: 1em;' type='text' name='search' value='".$_GET['search']."' id='searchProductsBar'>";
+		else
+			echo"<input style='width:98%;height:2em;font-size:1.2em;font-weight:bold;margin-top:0.5em;margin-left:2%;text-indent: 1em;' type='text' name='search' id='searchProductsBar'>";
+		echo"</td>
+		<td style='width:16%;text-align:right'>
+			<button type='submit' style='width:100%;height:2em;font-size:1.3em;font-weight:bold;margin-top:0.5em;padding:0;border:0;'>Search</button>
+		</td>
+		
+		</tr>
+	</table><hr>";
+/* 	echo"<table style='width:96%;margin-top:0.5em;'>";
 	
-	//begining of time table with today's times first
-	var hourTableString='<table style="border-style:groove;width:60%;">'+
-		'<tr><br></tr>'+
-		'<tr><td colspan="3" style="text-align:center;border-style:none;"><b><u><h2>Hours</h2></u></b></td></tr>'+
-		'<tr><td colspan="3" style="text-align:center;border-style:none;"><center><table><tr><td><b>Hours Today: '+timeText+'</b></td></tr></table></center></td></tr>';
-	for ($x = 1; $x <= 6; $x++) { //must start at 1 so first row after today is today+1
-		open = "open"+days[(today+$x)%7];
-		close = "close"+days[(today+$x)%7];
-		timeText = theTimeSlingingSlasher(data[open],data[close]);
-		hourTableString+='<tr><td colspan="3" style="text-align:center;border-style:none;"><center><table><tr><td>'+dayNames[(today+$x)%7]+': '+timeText+'</td></tr></table></center></td></tr>';
-	}
-	//end of the time table	
-	hourTableString+='</table>';
+		
+		$catQuery = "SELECT Category FROM Products GROUP BY Category";
+		$categories = (!is_null($catQuery))?mysql_query($catQuery):null;
+		
+		
+		$categories2 = array();
+		$allOption = array('Category' => 'All');
+		$categories2[] = $allOption;
+		if(!is_null($catQuery))
+			while ($row = mysql_fetch_array($categories))
+				$categories2[] = $row;
+		else
+			$categories2 = null; //categories2 is actually an assoc array instead of a mysql resource
+
+
+		//actually a very important variabble. figure out where it goes later		
+		$setCat = (isset($_GET['Category']))?$_GET['Category']:NULL;
+		
+		echo"<tr><td style='text-align:right;width:30%'>
+		<span style='height:2em;'>
+			Category: </span></td>
+			<td style='width:60%;'>
+			<select name='Category' onchange='this.form.submit()' id='Category' style='width:100%;text-align:center;box-sizing:border-box;-moz-box-sizing:border-box;height:2em;'>";
+		if(isset($_GET['Category']))// if the category is previously sent to server, then set it as top of dropdown
+			echo'<option value="'.$_GET['Category'].'">'.$_GET['Category'].'</option>';
+		foreach($categories2 as $uniqueEntry){
+			if(!(isset($_GET['Category']) && $uniqueEntry['Category']==$_GET['Category']))
+				echo'<option value="'.$uniqueEntry['Category'].'">'.$uniqueEntry['Category'].'</option>';
+		}
+		echo"</select>
+		        </td><td style='width:30%'></td>
+		      </tr></table>"; */
+	
+	echo"<table style='width:96%'>";
+		$searchMethod = array("Basic Price Results","ABV Price Comparison");
+		$numSearchMethod = 2; 
+		echo"<tr>
+		<td><center><div style='padding-bottom:1em;width:60%;'>";
+		//display method radiobuttons are set to previously GET-requested method
+		//changing them resubits the form with the updated information
+		if(!isset($_GET['searchMethod'])){
+			for($s=0;$s<$numSearchMethod;++$s) { 
+				if($s==0)
+					echo'<input type="radio" onchange="this.form.submit()" name="searchMethod" value="'.$searchMethod[0].'" checked>'.$searchMethod[0].'<br>';
+				else
+					echo'<input type="radio" onchange="this.form.submit()" name="searchMethod" value="'.$searchMethod[$s].'" >'.$searchMethod[$s].'<br>';
+			}
+		}else{
+			for($s=0;$s<$numSearchMethod;++$s) { 
+				if($_GET['searchMethod']==$searchMethod[$s])
+					echo'<input type="radio" onchange="this.form.submit()" name="searchMethod" value="'.$searchMethod[$s].'" checked>'.$searchMethod[$s].'<br>';
+				else
+					echo'<input type="radio" onchange="this.form.submit()" name="searchMethod" value="'.$searchMethod[$s].'" >'.$searchMethod[$s].'<br>';
+			}
+		}
+	echo"</div></center></td></tr>";	
+echo"</table>
+<center>
+<table style='background-color:white;width:98%;border-style:solid;border-color:black;border-width:3px'>";
+$productCounter = 0; //counts number of search results or just price results for the town. Allows coloring of rows and "0 results" alert message
+//if the searchMethod has been previously set, build that table of results
+if(!isset($_GET['searchMethod']) || $_GET['searchMethod'] == $searchMethod[0]){ //if the searchMethod is not set, set the table to basic search
+	echo"<tr style='vertical-align:top;text-align:center;height:1em;margin-right:auto;margin-left:auto;background-color:silver;color'>
+			<th style='width:6em;' onclick='sort_table(priceTable, 0, asc1); asc1 *= -1; asc2 = 1; asc3 = 1; asc4 = 1; asc5 = 1;asc6 = 1;'>Date</th>
+			<th onclick='sort_table(priceTable, 1, asc2); asc2 *= -1; asc3 = 1; asc1 = 1; asc4 = 1; asc5 = 1;asc6 = 1;'>Name</th>
+			<th onclick='sort_table(priceTable, 2, asc3); asc3 *= -1; asc1 = 1; asc2 = 1; asc4 = 1; asc5 = 1;asc6 = 1;'>Style</th>
+			<th onclick='sort_table(priceTable, 3, asc4); asc4 *= -1; asc1 = 1; asc2 = 1; asc3 = 1; asc5 = 1;asc6 = 1;'>Pack</th>
+			<th style='width:3em;' onclick='sort_table(priceTable, 4, asc5); asc5 *= -1; asc1 = 1; asc2 = 1; asc4 = 1; asc3 = 1;asc6 = 1;'>ABV</th>
+			<th style='width:4.2em;' onclick='sort_table(priceTable, 5, asc6); asc6 *= -1; asc1 = 1; asc2 = 1; asc4 = 1; asc5 = 1;asc3 = 1;'>Price</th>
+			</tr><tbody id='priceTable'>";
+		$query = "SELECT productID FROM `$town` WHERE vendorID = '$id' GROUP BY productID"; //1 for each productID
+
+
+		$prices = (!is_null($query))?mysql_query($query):null;
+	/*  	if (!$prices) {
+		    die('Could not query:' . mysql_error());
+		}  */
+		/*
+		*
+		* Here it bugs out when the town does not have a table.
+		* Insert price for that vendor or another vendor in that location
+		*
+		*/
+		$numProducts = (!is_null($prices))?mysql_numrows($prices):0;
+		$productArray = array();
+		for($i = 0; $i<=$numProducts-1; $i++)//this array_push only works for numeric arrays
+			array_push($productArray,mysql_result($prices,$i,"productID"));
+			  
+		foreach ($productArray as $pro){
 			
-	var wrapper = document.getElementById('info');
-	var topTray = document.createElement('div');
-	var hourTable = document.createElement('div');
+			//query to return price results from town table
+			$pQuery = "SELECT timestamp,price FROM $town 
+			WHERE vendorID = $id 
+			AND productID = $pro ORDER BY timestamp DESC LIMIT 1";
+			$proQuery = mysql_query($pQuery);
+			//query from the town database
+			$price=mysql_result($proQuery,0,"price"); 
+			$timestamp=mysql_result($proQuery,0,"timestamp");
+			
+			//query to return data about the product from the product table
+			$nameQuery = (isset($_GET['search']) && $_GET['search']!="")?
+				"SELECT Name,volume,quantity,cans,ABV,Style,Category FROM Products WHERE ID = $pro AND Name LIKE '%$_GET[search]%'"
+				:"SELECT Name,volume,quantity,cans,ABV,Style,Category FROM Products WHERE ID = $pro";
+			
+			
+			$proNameQuery = mysql_query($nameQuery);
+			if(mysql_num_rows($proNameQuery)==1){//a search result has been found
+				$productCounter+=1; //Keeps track of number of search results
+				//queries from the product database
+				$productName= mysql_result($proNameQuery,0,"Name"); //there is only 1 result for this product
+				$quantity=mysql_result($proNameQuery,0,"Quantity");
+				$volume=mysql_result($proNameQuery,0,"Volume"); 
+				$abv=mysql_result($proNameQuery,0,"ABV");
+				$style=mysql_result($proNameQuery,0,"Style");
+				$bc=(mysql_result($proNameQuery,0,"Cans")==1)?"Cans":"Bottles";
 
-	topTray.id = 'topTray';
-	hourTable.id = 'hourTable';
-
-	topTray.innerHTML = topTableString;
-	hourTable.innerHTML = hourTableString;
-	wrapper.appendChild(topTray);
-	wrapper.appendChild(hourTable);
-	wrapper.style.height = "100%";
-
-	//MARKER FOR STORE
-	var marker = new google.maps.Marker({
-	position: new google.maps.LatLng(data['latitude'], data['longitude']),
-	map: map});
-	var infoWindow= new google.maps.InfoWindow();
- 	marker.addListener('click', function() {
-		var contentString = '<div id="content">'+
-		'<h1 id="firstHeading" class="firstHeading">'+name+'</h1>'+
-		'<center><p>'+data['address']+', '+data['location']+' '+data['state']+'</p></center>'
-		'</div>';
-		infoWindow.setContent(contentString);
-		infoWindow.open(map, marker);  
-		map.setCenter(marker.getPosition());		
-	}); 
-}
-
-function theTimeSlingingSlasher(open,close){
-	var opening = new Date();
-	var closing = new Date();
-	//OPENING STUFF
-	var openHours = open.substring(0,2);
-	var openMin = open.substring(2);
-	opening.setHours(openHours,openMin,0); 
-	var openTimeText = (openHours % 12 == 0)?12:openHours%12;
-	openTimeText += ":";
-	openTimeText += (openMin <10)?openMin: openMin;
-	openTimeText += (openHours < 12)?" am":" pm";
-	//CLOSING STUFF
-	var closeHours = close.substring(0,2);
-	var closeMin = close.substring(2);
-	closing.setHours(closeHours,closeMin,0);
-	var closeTimeText = (closeHours % 12 == 0)?12:closeHours%12;
-	closeTimeText += ":";
-	closeTimeText += (closeMin <10)?closeMin: closeMin;
-	closeTimeText += (closeHours < 12)?" am":" pm";
-	//FINAL TIME STRING
-	return openTimeText + ' - ' + closeTimeText;
-}
-
-    </script>    
-<?php echo"
-<script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyBmv08kSZpkCuBwQEUsqqnxJ7m_ZBro_OQ&signed_in=true&libraries=places&callback=initMap' async defer></script>
-</center>
-</div>
-</div>
- 
-	</article>								
-	</main>";db_close1(); 
-	include 'footer.php';
-	//CLOSES DATABASE CONNECTION AT END
-?>the stuff to format the timestamp
+				//the stuff to format the timestamp
 				$dateOfInsertion = new dateTime;
 				$dateOfInsertion->setTimestamp(strtotime($timestamp));
 				//draws the data's row into the table
