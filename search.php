11637ed5265a834ddeb46d1e3a88d3bc9dde7c3f
Mason Hoffman <mhoff873@users.noreply.github.com>
2017-03-16 14:30:41 -0400

    Add files via upload

---
+++ search.php
@@ -0,0 +1,23 @@
+<?php 
+	include 'header.php';
+?>
+		
+		<main id="center" class="column">
+			<article>
+			
+				<h1>Search Coming soon...</h1>
+				<!--</p>
+				<a href=stateCollege.php>State College<a>
+				<a href=carlisle.php>Carlisle<a>
+				</p>
+				<h2>More Locations Coming soon</h2>
+				<p>Reading</p>
+				<p>Kutztown</p>
+				<p>Lewistown<p>
+				<p>Harrisburg</p>-->
+				
+			</article>								
+		</main>
+<?php
+	include 'footer.php';
+?>
